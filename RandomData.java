1bd10e64e6138d6d9cfcfb6d9878d6b84ec0260a
=Bi-Huynh <=huynhtrungduc1231998@gmail.com>
2019-05-25 00:52:00 +0700

    version 3
    thêm hàm random util.Date

--- RandomData.java
+++ RandomData.java
@@ -1,7 +1,11 @@
 package Random_Data_Virtual;
 
 import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -13,7 +17,7 @@ public class RandomData {
      * trả về 1 chuỗi hoa bất kỳ chuỗi trả về sẽ có độ dài là n + 1
      *
      * @param n là độ dài của chuỗi sẽ trả về
-     * 
+     *
      * @return chuỗi
      */
     public static String rChuoi(int n) {
@@ -149,7 +153,7 @@ public class RandomData {
         ngay = year + "-" + month + "-" + day;
         return Date.valueOf(ngay);
     }
-    
+
     /**
      * trả về 1 cái chuỗi chuẩn có kích thước là người dùng nhập vào chuỗi trả
      * về sẽ có độ dài là n + 1 n : số ký tự
@@ -158,7 +162,7 @@ public class RandomData {
      * thường
      *
      * @param lenght là độ dài của chuỗi sẽ trả về
-     * 
+     *
      * @return chuỗi
      */
     public static String rChuoiChuan(int lenght) {
@@ -172,41 +176,41 @@ public class RandomData {
         }
         return chuoi;
     }
-    
+
     /**
      * trả về 1 con số trong khoảng từ 0 -> Integer.MAX_VALUE
-     * 
+     *
      * @return Integer
-    */
-    public static Integer rInteger () {
+     */
+    public static Integer rInteger() {
         return new Random().nextInt(Integer.MAX_VALUE);
     }
-    
+
     /**
      * trả về 1 số trong khoảng từ <b>Min</b> đến <b>Max</b>
-     * 
+     *
      * @param min số nhỏ nhất có thể random được
      * @param max số lớn nhất có thể random được
-     * 
+     *
      * @return <b>Integer.MIN_VALUE</b> khi <b>Min</b> > <b>Max</b>
-    */
-    public static Integer rIntegerTo (int min, int max) {
+     */
+    public static Integer rIntegerTo(int min, int max) {
         if (max <= min) {
             return Integer.MIN_VALUE;
         }
         return new Random().nextInt(max - min + 1) + min;
     }
-    
+
     /**
      * trả về 1 con số chia hết cho số đã cho
-     * 
+     *
      * @param divisor : số muốn chia hết
-     * 
-     * số trả về nằm trong đoạn từ 0 -> 100000
-     * trong đoạn này số nào chia hết cho divisor sẽ được trả về
-     * 
+     *
+     * số trả về nằm trong đoạn từ 0 -> 100000 trong đoạn này số nào chia hết
+     * cho divisor sẽ được trả về
+     *
      * @return -1 divisor >= 100000
-    */
+     */
     public static Integer rIPercent(int divisor) {
         if (divisor >= 100000) {
             return -1;
@@ -218,17 +222,17 @@ public class RandomData {
         }
         return temp;
     }
-    
+
     /**
      * trả về 1 con số không chia hết cho số đã cho
-     * 
+     *
      * @param divisor : số muốn chia
-     * 
-     * số trả về nằm trong đoạn từ 0 -> 100000
-     * trong đoạn này số nào không chia hết cho divisor sẽ được trả về
-     * 
+     *
+     * số trả về nằm trong đoạn từ 0 -> 100000 trong đoạn này số nào không chia
+     * hết cho divisor sẽ được trả về
+     *
      * @return -1 divisor >= 100000
-    */
+     */
     public static Integer rNonIPercent(int divisor) {
         if (divisor >= 100000) {
             return -1;
@@ -240,29 +244,28 @@ public class RandomData {
         }
         return temp;
     }
-    
+
     /**
      * trả về 1 chuỗi họ tên ảo có dạng chuẩn chuỗi
-     * 
+     *
      * @return String name
-    */
-    public static String rName () {
+     */
+    public static String rName() {
         String name = rChuoiChuan(5) + ' ' + rChuoiChuan(5) + ' ' + rChuoiChuan(3);
         return name.trim();
     }
-    
+
     /**
      * trả về 1 chuỗi dành cho trường ID
-     * 
-     * @param _id là biến chuỗi mặc định 
-     * 
+     *
+     * @param _id là biến chuỗi mặc định
+     *
      * vd : _id = DH => id = DH + 99990 => DH99990
-     * 
-     * chuỗi số phía sau từ 1000 -> 100000
-     * tức là có 99990 số
-     * 
+     *
+     * chuỗi số phía sau từ 1000 -> 100000 tức là có 99990 số
+     *
      * @return id
-    */
+     */
     public static String rID(String _id) {
         String id = _id + (rIntegerTo(0, 100000) + 1000);
         return id;
@@ -270,28 +273,63 @@ public class RandomData {
 
     /**
      * random ra 1 số Float
-     * 
+     *
      * @return Float
-    */
+     */
     public static Float rNumberFloat() {
         return new Random().nextFloat();
     }
-    
+
     /**
      * random ra 1 số Double
-     * 
+     *
      * @return Double
-    */
+     */
     public static Double rNumberDouble() {
         return new Random().nextDouble();
     }
-    
+
     /**
      * random ra 1 Boolean
-     * 
+     *
      * @return Boolean
-    */
+     */
     public static Boolean rIsBoolean() {
         return new Random().nextBoolean();
+    }
+
+    /**
+     * hàm trả về 1 văn bản có n chữ
+     *
+     * @param n số chữ có trong text
+     *
+     * @return Text
+     */
+    public static String rText(int n) {
+        String text = "";
+        for (int i = 0; i < n; i++) {
+            text += rChuoiChuan(5) + ' ';
+        }
+        return text.trim();
+    }
+
+    /**
+     * trả về Date bất kỳ và ngày này nhỏ hơn hoặc bằng ngày hiện tại ngày nhỏ
+     * nhất là ngày 2005-1-1
+     * 
+     * random 1 ngày theo kiểu java.util.Date
+     * 
+     * @return java.util.Date
+    */
+    public static java.util.Date rDateUtil() {
+        SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
+
+        java.util.Date date = null;
+        try {
+            date = format.parse(rDateSQL().toString());
+        } catch (ParseException ex) {
+            Logger.getLogger(RandomData.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return date;
     }
 }
